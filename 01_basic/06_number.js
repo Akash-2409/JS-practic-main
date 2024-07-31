5c58c1da481c1f6301d57d35067dc685f8923d82
Akash-2409 <akashboda24@gmail.com>
2024-07-31 10:18:51 +0000

    number

---
+++ 01_basic/06_number.js
@@ -0,0 +1,11 @@
+const price = 100000000
+
+// console.log(price.toLocaleString("en-IN"));
+
+// console.log(Math.PI)
+
+const number = Math.random()
+
+console.log(number);
+console.log(number*10);
+console.log((number*10) + 1);
